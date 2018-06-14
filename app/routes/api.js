ad3539b9c12aa5a4d3d2ab9fc9f4f85a4aeb5b79
Alfredo Cano <alfrecanodiaz@gmail.com>
2018-06-14 15:53:08 -0400

    Routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/form', function(req, res) {
-    res.send("Called Form Method");
+router.get('/page/:id', function(req, res) {
+    res.send("The id you specified is " + req.params.id);
+});
+
+router.get('/page/:name/:id', function(req, res) {
+    res.send("name: " + req.params.name + " id: " + req.params.id);
 });
 
 //export this router to use in index.js
--- index.js
+++ index.js
@@ -4,9 +4,25 @@ var app = express();
 var web_routes = require('./app/routes/web.js');
 var api_routes = require('./app/routes/api.js');
 
+//Middleware function to log request protocol
+app.use('/web', function(req, res, next) {
+    console.log("A request for things received at {web} " + Date.now());
+    next();
+ });
+
+ app.use('/api', function(req, res, next) {
+    console.log("A request for things received at {api} " + Date.now());
+    next();
+ });
+
 //first parameter is group route
 //it will be called like /express/home
 app.use('/web', web_routes);
 app.use('/api', api_routes);
+
+//default not found route error
+app.get('*', function(req, res) {
+    res.send('Sorry, this is an invalid URL.');
+});
 
 app.listen(3000);
